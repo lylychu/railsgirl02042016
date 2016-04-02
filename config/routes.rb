2dcfbbcb2af54bc365a28deaed3f43bd3e3c4437
Lyly <chuly0112@gmail.com>
2016-04-02 08:39:45 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
+  root :to => redirect('/ideas')
+  resources :ideas
+  resources :comments
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
